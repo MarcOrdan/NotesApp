43e01051318405fe778279ea5a6430ebac99975e
Mac Ordan <marcordan@gmail.com>
2019-08-16 16:46:19 +0800

    create other commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -11,13 +11,19 @@ yargs.command({
     describe: 'Add a new note.',
     builder: {
         title: {
-            describe: 'Note title',
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
             demandOption: true,
             type: 'string'
         }
     },
     handler: function(argv){
-        console.log('Title: ' + argv.title);
+        console.log('Title: ' + argv.title );
+        console.log('Title: ' + argv.body );
     }
 })
 
