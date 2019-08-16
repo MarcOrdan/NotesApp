59aaebe47e7c37236f22c8eeb413e08706f33e69
Mac Ordan <marcordan@gmail.com>
2019-08-16 11:01:08 +0800

    Added yargs parser

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-console.log(chalk.bold.white.bgGreen.underline('Success!'));
+//customize yargs version
+yargs.version('1.1.0');
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    handler: function(){
+        console.log('Adding a new note.');
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note.',
+    handler: function(){
+        console.log('Removing the note.');
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes.',
+    handler: function(){
+        console.log('Listing out all notes.');
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read the notes.',
+    handler: function(){
+        console.log('Reading the notes.');
+    }
+})
+
+console.log(yargs.argv);
